ed91f15389961b67a18245a9e6ea5cc6304ebe56
Andrew Alexeev <166375060+Quentinious@users.noreply.github.com>
2025-04-06 20:11:50 +0700

    Update task1.h

--- task1.h
+++ task1.h
@@ -1,1 +1,22 @@
+#ifndef HEADER_H
+#define HEADER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <libgen.h>
+
+#define PAGESIZE 4096
+
+
+
+
+void copyFilesAndReversed(const char *from, const char *to);
+void ReverseOrder(char *s);
+void DirectoryCopy(const char *src, const char *dst);
+
+
+#endif
