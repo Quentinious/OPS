8a9f1b484460a5b84ada6d00d1b8f21bf5b86e98
Andrew Alexeev <166375060+Quentinious@users.noreply.github.com>
2025-05-21 13:07:09 +0700

    Update cde_page.c

--- lab5/task2/cde_page.c
+++ lab5/task2/cde_page.c
@@ -3,65 +3,93 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/mman.h>
+#include <setjmp.h>
 
 #define PAGE_COUNT 10
+
+void *region;
+size_t page_size;
+char *ptr;
+sigjmp_buf jump_buffer;
 
 void sigHandler(int sig)
 {
     printf("SIGSEGV caught! Access violation occurred.\n");
-    exit(EXIT_FAILURE);
+    siglongjmp(jump_buffer, 1); 
 }
 
-int main() 
+int main()
 {
     
     printf("PID: %d\n", getpid());
-    printf("Open another terminal and run: cat /proc/%d/maps\n", getpid());
+    printf("Run: cat /proc/%d/maps\n", getpid());
     sleep(10); 
-    size_t page_size = getpagesize();
-    void *region = mmap(NULL, PAGE_COUNT * page_size,
-                        PROT_NONE,
-                        MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    
+    page_size = getpagesize();
+    region = mmap(NULL, PAGE_COUNT * page_size,
+                  PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
-    if (region == MAP_FAILED)
-    {
-        printf("mmap failed");
+    if (region == MAP_FAILED) {
+        perror("mmap failed");
         exit(EXIT_FAILURE);
     }
 
+    ptr = (char *)region;
     printf("Memory mapped at: %p\n", region);
-    sleep(15); 
+    sleep(10); 
     signal(SIGSEGV, sigHandler);
 
+    
     printf("\n--- Testing PROT_WRITE only (no read) ---\n");
     if (mprotect(region, PAGE_COUNT * page_size, PROT_WRITE) != 0) 
     {
-        printf("mprotect (write only)");
+        perror("mprotect (write only)");
         exit(EXIT_FAILURE);
     }
 
-    char *ptr = (char *)region;
-    printf("Trying to read (should cause SIGSEGV)...\n");
-    char val = ptr[0]; 
-    printf("Value: %c\n", val); 
+    
+    if (sigsetjmp(jump_buffer, 1) == 0) 
+    {
+        printf("Trying to read (should cause SIGSEGV)...\n");
+        char val = ptr[5];
+        printf("Value: %c\n", val); 
+    } else {
+        printf("Recovered from read violation.\n");
+    }
+
+    sleep(5);
+
+    
     printf("\n--- Testing PROT_READ only (no write) ---\n");
-    if (mprotect(region, PAGE_COUNT * page_size, PROT_READ) != 0) {
-        printf("mprotect (read only)");
+    if (mprotect(region, PAGE_COUNT * page_size, PROT_READ) != 0) 
+    {
+        perror("mprotect (read only)");
         exit(EXIT_FAILURE);
     }
 
-    printf("Trying to write (should cause SIGSEGV)...\n");
-    ptr[1] = 'A'; 
-    printf("Written!\n"); 
-    sleep(15);
-    if (munmap((char *)region + 4 * page_size, 3 * page_size) != 0) {
-        printf("munmap failed");
+    if (sigsetjmp(jump_buffer, 1) == 0) 
+    {
+        printf("Trying to write (should cause SIGSEGV)...\n");
+        ptr[1] = 'A'; 
+        printf("Written!\n"); 
+    } else
+    {
+        printf("Recovered from write violation.\n");
+    }
+
+    sleep(10);
+
+    
+    printf("\n--- Unmapping pages 4 to 6 ---\n");
+    if (munmap(ptr + 4 * page_size, 3 * page_size) != 0) 
+    {
+        perror("munmap failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Pages 4-6 unmapped.\n");
-    sleep(15);
-    printf("Check memory map again.\n");
+    sleep(10); 
 
+    printf("Done. Exiting.\n");
     return 0;
 }
