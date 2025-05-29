c115feadc5020fd32e7781fb1ddb1006f2e03d8c
Andrew Alexeev <166375060+Quentinious@users.noreply.github.com>
2025-05-29 16:45:41 +0700

    Update tcp_client.c

--- task2/tcp_client.c
+++ task2/tcp_client.c
@@ -1,4 +1,3 @@
-// tcp_echo_client.c
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,28 +15,18 @@ int main()
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
 
-    // Создание TCP-сокета
+    
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Настройка адреса сервера
+    
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
-
-    // Подключение к серверу
-
-    // connect() пытается установить TCP-соединение с сервером по адресу server_addr.
-    // Параметры:
-    // sockfd — дескриптор клиента.
-    // (struct sockaddr *)&server_addr — указатель на адрес сервера.
-    // sizeof(server_addr) — размер структуры адреса.
-    // Если соединение не удаётся, программа завершается.
-
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         perror("connect failed");
@@ -52,22 +41,9 @@ int main()
         printf("Введите сообщение: ");
         if (!fgets(buffer, BUFFER_SIZE, stdin))
             break;
+        write(sockfd, buffer, strlen(buffer)); 
 
-        //         write() отправляет введённые данные серверу через сокет.
-
-        // Параметры:
-
-        // sockfd — дескриптор сокета.
-
-        // buffer — указатель на данные.
-
-        // strlen(buffer) — длина строки.
-
-        // Блокирует процесс до полной отправки данных.
-
-        write(sockfd, buffer, strlen(buffer)); // отправка
-
-        ssize_t n = read(sockfd, buffer, BUFFER_SIZE); // получение
+        ssize_t n = read(sockfd, buffer, BUFFER_SIZE); 
         if (n <= 0)
             break;
 
@@ -78,16 +54,3 @@ int main()
     close(sockfd);
     return 0;
 }
-
-
-// Итог
-// Клиент создаёт TCP-сокет.
-
-// Подключается к серверу по IP и порту.
-
-// В цикле отправляет введённые сообщения.
-
-// Получает от сервера их же (эхо).
-
-// Работает, пока не будет EOF на вводе или соединение не закроется.
-
